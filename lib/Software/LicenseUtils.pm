a47eda5c428a583e22a58b23d852811c50b0d05a
Ricardo SIGNES <rjbs@cpan.org>
2008-04-11 11:43:47 -0400

    update docs

--- lib/Software/LicenseUtils.pm
+++ lib/Software/LicenseUtils.pm
@@ -10,30 +10,43 @@ Software::LicenseUtils - little useful bits of code for licensey things
 
 =head2 license_from_pm
 
-  my $license_class = Software::LicenseUtils->guess_license_from_pm($pm_text);
+  my @guesses = Software::LicenseUtils->guess_license_from_pm($pm_text);
 
 Given text containing POD, like a .pm file, this method will attempt to guess
 at the license under which the code is available.  This method will either
-return a Software::License class, Software::License object, or undef.
+a list of Software::License classes (or instances) or false.
+
+Calling this method in scalar context is a fatal error.
 
 =cut
 
+my $_v = qr/(?:v(?:er(?:sion|\.))(?: |\.)?)/i;
 my @phrases = (
+  "under the same (?:terms|license) as perl $_v?6" => [],
   'under the same (?:terms|license) as perl' => 'Perl_5',
-  'GNU public license'                       => 'GPL_2_0',
-  'GNU lesser public license'                => 'LGPL_2_0',
-  'BSD license'                              => 'BSD',
-  'Artistic license'                         => 'Artistic_1_0',
-  'GPL'                                      => 'GPL',
-  'LGPL'                                     => 'LGPL_2_0',
-  'BSD'                                      => 'BSD',
-  'Artistic'                                 => 'Artistic_1_0',
-  'MIT'                                      => 'MIT',
-  'proprietary'                              => 'proprietary',
+  "GNU public license $_v?([123])"           => sub { "GPL_$_[0]_0" },
+  'GNU public license'                       => [ map {"GPL_$_\_0"} (1..3) ],
+  "GNU lesser public license $_v?([23])\\D"  => sub {
+    $_[0] == 2 ? 'LGPL_2_1' : $_[0] == 3 ? 'LGPL_3_0' : ()
+  },
+  'GNU lesser public license'  => [ qw(LGPL_2_1 LGPL_3_0) ],
+  'BSD license'                => 'BSD',
+  "Artistic license $_v?(\\d)" => sub { "Artistic_$_[0]_0" },
+  'Artistic license'           => [ map { "Artistic_$_\_0" } (1..2) ],
+  "GPL $_v?(\\d)"              => sub { "GPL_$_[0]_0" },
+  'GPL'                        => [ map { "GPL_$_\_0" } (1..3) ],
+  "LGPL $_v?(\\d)"             => sub {
+    $_[0] == 2 ? 'LGPL_2_1' : $_[0] == 3 ? 'LGPL_3_0' : ()
+  },
+  'LGPL'                       => [ qw(LGPL_2_1 LGPL_3_0) ],
+  'BSD'                        => 'BSD',
+  'Artistic'                   => [ map { "Artistic_$_\_0" } (1..2) ],
+  'MIT'                        => 'MIT',
 );
 
-sub guess_license_from_pm {
+sub guess_license_from_pod {
   my ($class, $pm_text) = @_;
+  die "can't call guess_license_* in scalar context" unless wantarray;
 
 	if (
 		$pm_text =~ m/
@@ -51,16 +64,54 @@ sub guess_license_from_pm {
     for (my $i = 0; $i < @phrases; $i += 2) {
       my ($pattern, $license) = @phrases[ $i .. $i+1 ];
 			$pattern =~ s{\s+}{\\s+}g;
-			if ( $license_text =~ /\b$pattern\b/i ) {
+			if ( $license_text =~ /$pattern/i ) {
+        my $match = $1;
 				# if ( $osi and $license_text =~ /All rights reserved/i ) {
 				# 	warn "LEGAL WARNING: 'All rights reserved' may invalidate Open Source licenses. Consider removing it.";
 				# }
-				return "Software::License::$license";
+        my @result = (ref $license||'') eq 'CODE'  ? $license->($match)
+                   : (ref $license||'') eq 'ARRAY' ? @$license
+                   :                                 $license;
+
+        return unless @result;
+				return map { "Software::License::$_" } @result;
 			}
 		}
 	}
 
 	return;
+}
+
+my %yaml_keys = (
+  perl         => 'Perl_5',
+  apache       => [ map { "Apache_$_" } qw(1_1 2_0) ],
+  artistic     => 'Artistic_1_0',
+  artistic_2   => 'Artistic_2_0',
+  lgpl         => [ map { "LGPL_$_" } qw(2_1 3_0) ],
+  bsd          => 'BSD',
+  gpl          => [ map { "GPL_$_" } qw(1_0 2_0 3_0) ],
+  mit          => 'MIT',
+  mozilla      => [ map { "Mozilla_$_" } qw(1_0 1_1) ],
+);
+
+=head2 license_from_meta_yml
+
+  my @guesses = Software::LicenseUtils->guess_license_from_meta_yml($yaml_str);
+
+Given text containing POD, like a .pm file, this method will attempt to guess
+at the license under which the code is available.  This method will either
+a list of Software::License classes (or instances) or false.
+
+=cut
+
+sub guess_license_from_meta_yml {
+  my ($class, $yaml_text) = @_;
+  die "can't call guess_license_* in scalar context" unless wantarray;
+
+  my ($license_text) = $yaml_text =~ m{license: (.+)}g;
+
+  return unless $license_text and my $license = $yaml_keys{ $license_text };
+  return "Software::License::$license";
 }
 
 1;
--- t/utils.t
+++ t/utils.t
@@ -1,9 +1,10 @@
 use strict;
 use warnings;
-use Test::More tests => 1;
+use Test::More tests => 2;
 use Software::LicenseUtils;
 
-my $fake_pm = <<'END_PM';
+{
+    my $fake_pm = <<'END_PM';
 
 "magic true value";
 __END__
@@ -16,6 +17,48 @@ This is released under the same terms as perl itself.
 
 END_PM
 
-my $guess = Software::LicenseUtils->guess_license_from_pm($fake_pm);
+    my @guesses = Software::LicenseUtils->guess_license_from_pod($fake_pm);
 
-is($guess, 'Software::License::Perl_5', "guessed okay");
+    is_deeply(
+      \@guesses,
+      [ 'Software::License::Perl_5' ],
+      "guessed okay"
+    );
+}
+
+{
+    my $fake_yaml = <<'END_YAML';
+---
+abstract: 'packages that provide templated software licenses'
+author:
+  - 'Ricardo Signes <rjbs@cpan.org>'
+distribution_type: module
+generated_by: 'Module::Install version 0.71'
+license: perl
+meta-spec:
+  url: http://module-build.sourceforge.net/META-spec-v1.3.html
+  version: 1.3
+name: Software-License
+no_index:
+  directory:
+    - inc
+    - t
+requires:
+  Class::ISA: 0.000
+  Sub::Install: 0.000
+  Text::Template: 0.000
+  perl: 5.6.0
+tests: 't/*.t xt/*.t'
+version: 0.002
+END_YAML
+
+    my @guesses = Software::LicenseUtils->guess_license_from_meta_yml(
+      $fake_yaml
+    );
+
+    is_deeply(
+      \@guesses,
+      [ 'Software::License::Perl_5' ],
+      "guessed okay"
+    );
+}

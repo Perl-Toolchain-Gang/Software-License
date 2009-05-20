d7548b50743b3601ab21d37ac25b6ac8e098707b
Ricardo SIGNES <rjbs@cpan.org>
2009-05-19 21:23:06 -0400

    tweak tests

--- lib/Software/LicenseUtils.pm
+++ lib/Software/LicenseUtils.pm
@@ -23,7 +23,7 @@ Calling this method in scalar context is a fatal error.
 my $_v = qr/(?:v(?:er(?:sion|\.))(?: |\.)?)/i;
 my @phrases = (
   "under the same (?:terms|license) as perl $_v?6" => [],
-  'under the same (?:terms|license) as perl'    => 'Perl_5',
+  'under the same (?:terms|license) as (?:the )?perl'    => 'Perl_5',
   'affero g'                                    => 'AGPL_3',
   "GNU (?:general )?public license,? $_v?([123])" => sub { "GPL_$_[0]" },
   'GNU (?:general )?public license'             => [ map {"GPL_$_"} (1..3) ],
--- xt/release/pod-coverage.t
+++ xt/release/pod-coverage.t
@@ -3,4 +3,7 @@
 use Test::More;
 eval "use Test::Pod::Coverage 1.04";
 plan skip_all => "Test::Pod::Coverage 1.04 required for testing POD coverage" if $@;
-all_pod_coverage_ok({ coverage_class => 'Pod::Coverage::CountParents' });
+all_pod_coverage_ok({
+  coverage_class => 'Pod::Coverage::CountParents',
+  trustme => [ qw(meta_yml_name) ],
+});

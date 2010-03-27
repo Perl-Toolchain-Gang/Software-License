5542951c34ddb51642b97ae3b68be789a21c9bc4
Ricardo Signes <rjbs@cpan.org>
2010-03-26 20:56:53 -0400

    fix prereq, bump version

--- lib/Software/License.pm
+++ lib/Software/License.pm
@@ -9,11 +9,11 @@ Software::License - packages that provide templated software licenses
 
 =head1 VERSION
 
-version 0.014
+version 0.015
 
 =cut
 
-our $VERSION = '0.014';
+our $VERSION = '0.015';
 
 use Data::Section -setup => { header_re => qr/\A__([^_]+)__\Z/ };
 use Sub::Install ();

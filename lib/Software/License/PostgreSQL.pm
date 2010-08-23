0184283244c7163a7450acb8b575a9199636a3ef
Ricardo Signes <rjbs@cpan.org>
2010-08-22 23:17:22 -0400

    bring PostgreSQL in line with Software::License

--- lib/Software/License/PostgreSQL.pm
+++ lib/Software/License/PostgreSQL.pm
@@ -7,27 +7,28 @@ use base 'Software::License';
 sub name { 'The PostgreSQL License' }
 sub url  { 'http://www.opensource.org/licenses/postgresql' }
 
-sub meta_name  { 'postgresql' }
+sub meta_name  { 'open_source' }
+sub meta2_name { 'open_source' }
 
 1;
 __DATA__
 __LICENSE__
 The PostgreSQL License
 
-Copyright (c) $YEAR, $ORGANIZATION
+Copyright (c) {{$self->year}}, {{$self->holder}}
 
 Permission to use, copy, modify, and distribute this software and its
 documentation for any purpose, without fee, and without a written agreement is
 hereby granted, provided that the above copyright notice and this paragraph
 and the following two paragraphs appear in all copies.
 
-IN NO EVENT SHALL $ORGANISATION BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+IN NO EVENT SHALL {{$self->holder}} BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
 SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
-OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF $ORGANISATION
-HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+{{$self->holder}} HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-$ORGANISATION SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+{{$self->holder}} SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
 LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS,
-AND $ORGANISATION HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
-ENHANCEMENTS, OR MODIFICATIONS.
+AND {{$self->holder}} HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT,
+UPDATES, ENHANCEMENTS, OR MODIFICATIONS.

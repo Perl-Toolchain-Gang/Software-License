b30c61b88ffeed6cffc61d0727d3eaa686f20731
Ricardo Signes <rjbs@cpan.org>
2012-12-08 16:14:50 -0500

    mozilla_2_0 is not a valid meta v2 key

--- lib/Software/License/Mozilla_2_0.pm
+++ lib/Software/License/Mozilla_2_0.pm
@@ -7,7 +7,7 @@ use base 'Software::License';
 sub name { 'Mozilla Public License Version 2.0' }
 sub url  { 'http://www.mozilla.org/MPL/2.0/index.txt' }
 sub meta_name  { 'mozilla' }
-sub meta2_name { 'mozilla_2_0' }
+sub meta2_name { 'open_source' }
 
 1;
 __DATA__

127491183f305985dad08a85b61aee00b4092968
bowtie <bowtie@cpan.org>
2013-10-22 21:31:09 +0100

    background

--- lib/Software/LicenseUtils.pm
+++ lib/Software/LicenseUtils.pm
@@ -3,12 +3,16 @@ use warnings;
 package Software::LicenseUtils;
 # ABSTRACT: little useful bits of code for licensey things
 
+use File::Spec;
+use IO::Dir;
+use Module::Load;
+
 =method guess_license_from_pod
 
-  my @guesses = Software::LicenseUtils->guess_license_from_pod($pm_text);
+my @guesses = Software::LicenseUtils->guess_license_from_pod($pm_text);
 
 Given text containing POD, like a .pm file, this method will attempt to guess
-at the license under which the code is available.  This method will either
+at the license under which the code is available. This method will either
 a list of Software::License classes (or instances) or false.
 
 Calling this method in scalar context is a fatal error.
@@ -18,84 +22,93 @@ Calling this method in scalar context is a fatal error.
 my $_v = qr/(?:v(?:er(?:sion|\.))(?: |\.)?)/i;
 my @phrases = (
   "under the same (?:terms|license) as perl $_v?6" => [],
-  'under the same (?:terms|license) as (?:the )?perl'    => 'Perl_5',
-  'affero g'                                    => 'AGPL_3',
+  'under the same (?:terms|license) as (?:the )?perl' => 'Perl_5',
+  'affero g' => 'AGPL_3',
   "GNU (?:general )?public license,? $_v?([123])" => sub { "GPL_$_[0]" },
-  'GNU (?:general )?public license'             => [ map {"GPL_$_"} (1..3) ],
-  "GNU (?:lesser|library) (?:general )?public license,? $_v?([23])\\D"  => sub {
+  'GNU (?:general )?public license' => [ map {"GPL_$_"} (1..3) ],
+  "GNU (?:lesser|library) (?:general )?public license,? $_v?([23])\\D" => sub {
     $_[0] == 2 ? 'LGPL_2_1' : $_[0] == 3 ? 'LGPL_3_0' : ()
   },
-  'GNU (?:lesser|library) (?:general )?public license'  => [ qw(LGPL_2_1 LGPL_3_0) ],
-  'BSD license'                => 'BSD',
+  'GNU (?:lesser|library) (?:general )?public license' => [ qw(LGPL_2_1 LGPL_3_0) ],
+  'BSD license' => 'BSD',
   "Artistic license $_v?(\\d)" => sub { "Artistic_$_[0]_0" },
-  'Artistic license'           => [ map { "Artistic_$_\_0" } (1..2) ],
-  "LGPL,? $_v?(\\d)"             => sub {
+  'Artistic license' => [ map { "Artistic_$_\_0" } (1..2) ],
+  "LGPL,? $_v?(\\d)" => sub {
     $_[0] == 2 ? 'LGPL_2_1' : $_[0] == 3 ? 'LGPL_3_0' : ()
   },
-  'LGPL'                       => [ qw(LGPL_2_1 LGPL_3_0) ],
-  "GPL,? $_v?(\\d)"              => sub { "GPL_$_[0]_0" },
-  'GPL'                        => [ map { "GPL_$_\_0" } (1..3) ],
-  'BSD'                        => 'BSD',
-  'Artistic'                   => [ map { "Artistic_$_\_0" } (1..2) ],
-  'MIT'                        => 'MIT',
+  'LGPL' => [ qw(LGPL_2_1 LGPL_3_0) ],
+  "GPL,? $_v?(\\d)" => sub { "GPL_$_[0]_0" },
+  'GPL' => [ map { "GPL_$_\_0" } (1..3) ],
+  'BSD' => 'BSD',
+  'Artistic' => [ map { "Artistic_$_\_0" } (1..2) ],
+  'MIT' => 'MIT',
 );
+
+my %meta_keys = ();
+
+# find all known Software::License::* modules and get identification data
+for my $lib ( map { "$_/Software/License" } @INC ) {
+    next unless -d $lib;
+    for my $file ( IO::Dir->new($lib)->read ) {
+        next unless $file =~ m{\.pm$};
+        # if it fails, ignore it
+        eval {
+            (my $mod = $file ) =~ s{\.pm$}{};
+            my $class = "Software::License::$mod";
+            load $class;
+            $meta_keys{ $class->meta_name }{$mod} = undef;
+            $meta_keys{ $class->meta2_name }{$mod} = undef;
+            my $name = $class->name;
+            unshift @phrases, qr/\Q$name\E/, [ $mod ];
+        }
+    }
+}
 
 sub guess_license_from_pod {
   my ($class, $pm_text) = @_;
   die "can't call guess_license_* in scalar context" unless wantarray;
 
-	if (
-		$pm_text =~ m/
-      (
-        =head \d \s+
-        (?:licen[cs]e|licensing|copyright|legal)\b
-        .*?
-      )
-      (=head\\d.*|=cut.*|)
-      \z
-    /ixms
+if (
+$pm_text =~ m/
+(
+=head \d \s+
+(?:licen[cs]e|licensing|copyright|legal)\b
+.*?
+)
+(=head\\d.*|=cut.*|)
+\z
+/ixms
   ) {
-		my $license_text = $1;
+my $license_text = $1;
 
     for (my $i = 0; $i < @phrases; $i += 2) {
       my ($pattern, $license) = @phrases[ $i .. $i+1 ];
-			$pattern =~ s{\s+}{\\s+}g;
-			if ( $license_text =~ /$pattern/i ) {
+$pattern =~ s{\s+}{\\s+}g
+unless ref $pattern eq 'Regexp';
+if ( $license_text =~ /$pattern/i ) {
         my $match = $1;
-				# if ( $osi and $license_text =~ /All rights reserved/i ) {
-				# 	warn "LEGAL WARNING: 'All rights reserved' may invalidate Open Source licenses. Consider removing it.";
-				# }
-        my @result = (ref $license||'') eq 'CODE'  ? $license->($match)
+# if ( $osi and $license_text =~ /All rights reserved/i ) {
+# warn "LEGAL WARNING: 'All rights reserved' may invalidate Open Source licenses. Consider removing it.";
+# }
+        my @result = (ref $license||'') eq 'CODE' ? $license->($match)
                    : (ref $license||'') eq 'ARRAY' ? @$license
-                   :                                 $license;
+                   : $license;
 
         return unless @result;
-				return map { "Software::License::$_" } @result;
-			}
-		}
-	}
-
-	return;
+return map { "Software::License::$_" } @result;
+}
+}
 }
 
-my %meta_keys = (
-  perl         => 'Perl_5',
-  apache       => [ map { "Apache_$_" } qw(1_1 2_0) ],
-  artistic     => 'Artistic_1_0',
-  artistic_2   => 'Artistic_2_0',
-  lgpl         => [ map { "LGPL_$_" } qw(2_1 3_0) ],
-  bsd          => 'BSD',
-  gpl          => [ map { "GPL_$_" } qw(1 2 3) ],
-  mit          => 'MIT',
-  mozilla      => [ map { "Mozilla_$_" } qw(1_0 1_1 2_0) ],
-);
+return;
+}
 
 =method guess_license_from_meta
 
-  my @guesses = Software::LicenseUtils->guess_license_from_meta($meta_str);
+my @guesses = Software::LicenseUtils->guess_license_from_meta($meta_str);
 
 Given the content of the META.(yml|json) file found in a CPAN distribution, this
-method makes a guess as to which licenses may apply to the distribution.  It
+method makes a guess as to which licenses may apply to the distribution. It
 will return a list of zero or more Software::License instances or classes.
 
 =cut
@@ -108,9 +121,11 @@ sub guess_license_from_meta {
 
   return unless $license_text and my $license = $meta_keys{ $license_text };
 
-  return map { "Software::License::$_" } ref $license ? @$license : $license;
+  return map { "Software::License::$_" } sort keys %$license;
 }
 
 *guess_license_from_meta_yml = \&guess_license_from_meta;
+
+
 
 1;

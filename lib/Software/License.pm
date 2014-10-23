b1ee496d850cca9af4adb418ab7e2494a1a3cb48
Dave Rolsky <autarch@urth.org>
2014-10-23 12:08:11 -0500

    Fix link to CC0_1_0 license

--- lib/Software/License.pm
+++ lib/Software/License.pm
@@ -174,7 +174,7 @@ The specific license:
 * L<Software::License::Artistic_1_0>
 * L<Software::License::Artistic_2_0>
 * L<Software::License::BSD>
-* L<Software::License::CC0>
+* L<Software::License::CC0_1_0>
 * L<Software::License::FreeBSD>
 * L<Software::License::GFDL_1_2>
 * L<Software::License::GPL_1>

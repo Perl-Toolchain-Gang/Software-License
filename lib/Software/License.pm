eeab1785221a3e0028ce7c27191d8f76ac2de06d
David E. Wheeler <david@justatheory.com>
2015-02-25 15:52:08 -0800

    Link to missing licenses.

--- lib/Software/License.pm
+++ lib/Software/License.pm
@@ -181,6 +181,7 @@ The specific license:
 * L<Software::License::Artistic_2_0>
 * L<Software::License::BSD>
 * L<Software::License::CC0_1_0>
+* L<Software::License::Custom>
 * L<Software::License::FreeBSD>
 * L<Software::License::GFDL_1_2>
 * L<Software::License::GPL_1>
@@ -195,6 +196,7 @@ The specific license:
 * L<Software::License::None>
 * L<Software::License::OpenSSL>
 * L<Software::License::Perl_5>
+* L<Software::License::PostgreSQL>
 * L<Software::License::QPL_1_0>
 * L<Software::License::SSLeay>
 * L<Software::License::Sun>
